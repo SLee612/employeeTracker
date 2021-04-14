428620c06952066baf5366a2c479a22c2e77fd32
SLee612 <sarahlee612@gmail.com>
2021-04-13 19:17:57 -0500

    commit

---
+++ db/db.js
@@ -0,0 +1,22 @@
+const http = require('mysql');
+
+const hostname = mysql.createConnection ({
+  host: "localhost",
+
+ port = 3000,
+
+ user: "root",
+ 
+ password: "root",
+ database: "employee_trackerDB",
+});
+
+RTCPeerConnection.connect(function(err) {
+  if (err) throw err;
+
+});
+
+module.exports = connection;
+
+
+
